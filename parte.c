677814f8644f015137e3fe94cdfc8fa815d34886
haoche <102539103+marschen233@users.noreply.github.com>
2023-10-03 16:08:57 -0400

    parte.c

--- parte.c
+++ parte.c
@@ -4,79 +4,66 @@
  * Created: 2023/10/2 20:51:43
  *  Author: haoche
  */ 
-#include <avr/io.h>
-#include <avr/interrupt.h>
-#include <util/delay.h>
-#include "uart.h"
-
 #define F_CPU 16000000UL
 #define BAUD_RATE 9600
 #define BAUD_PRESCALER (((F_CPU / (BAUD_RATE * 16UL))) - 1)
 
+#include <stdlib.h>
+#include <avr/io.h>
+#include <avr/interrupt.h>
+#include <util/delay.h>
+#include <string.h>
+#include "uart.h"
+
+#define BUTTON_PIN PIND2
 #define RED_LED PB1
 #define WHITE_LED PB2
-#define BUTTON_PIN PIND2
 
-// Timing definitions
+// Timing Definitions
 #define DOT_MIN 30
 #define DOT_MAX 200
 #define DASH_MIN 200
 #define DASH_MAX 400
-#define SPACE_MIN 400
+#define SPACE_MIN 400  // For simplicity, only one threshold for space detection
 
-//A-Z
-char* morse_alphabet[] = {
+char sequence[6] = {0};  // Store the sequence of dots, dashes, and spaces
+
+// Morse code definitions A-Z
+const char* morse_alphabet[26] = {
 	".-", "-...", "-.-.", "-..", ".", "..-.", "--.", "....", "..",
 	".---", "-.-", ".-..", "--", "-.", "---", ".--.", "--.-", ".-.",
 	"...", "-", "..-", "...-", ".--", "-..-", "-.--", "--.."
 };
 
-//0-9
-char* morse_numbers[] = {
-	"-----", ".----", "..---", "...--", "....-", ".....",
-	"-....", "--...", "---..", "----."
+// Morse code definitions 0-9
+const char* morse_numbers[10] = {
+	"-----", ".----", "..---", "...--", "....-", ".....", "-....",
+	"--...", "---..", "----."
 };
 
-volatile int button_press_time_count = 0;
-volatile int button_release_time_count = 0;
-unsigned char last_button_state = 0;
-
-void init();
 void decode_morse(char *sequence);
-char sequence[6] = {0};
-int idx = 0;
+
 int main(void) {
-	
-	init();
-	
-	while(1) {
-		if (sequence[idx] == ' ') { // Sequence complete
-			sequence[idx] = 0;  // Null terminate
-			decode_morse(sequence);
-			idx = 0;  // Reset for next sequence
-		}
-	}
-
-	return 0;
-}
-
-void init() {
-	// Initialize UART
 	UART_init(BAUD_PRESCALER);
-
-	// Initialize LEDs
+	
+	// Set PIND2 as input and PB1, PB2 as outputs
+	DDRD &= ~(1 << BUTTON_PIN);
 	DDRB |= (1 << RED_LED) | (1 << WHITE_LED);
-	DDRD &= ~(1 << BUTTON_PIN);  // Set as input
-
-	// Timer setup for timing checks
-	TCCR1B |= (1 << WGM12) | (1 << CS10);  // CTC mode, No prescaler
+	
+	// Timer initialization for every 1ms
+	TCCR1B |= (1 << WGM12) | (1 << CS12);  // CTC mode, prescaler 256
+	OCR1A = 62;  // Top value for 1ms
 	TIMSK1 |= (1 << OCIE1A);  // Enable timer compare interrupt
-	OCR1A = 16000;  // Set for 1ms @ 16MHz
 	sei();  // Enable global interrupts
+
+	while (1) {
+		// All action is happening in the ISR and the functions it calls
+	}
 }
 
 void decode_morse(char *sequence) {
 	char output[2] = {0, 0};  // Output buffer for single character conversion
+
 	//loop to print A-Z
 	for (int i = 0; i < 26; i++) {
 		if (strcmp(sequence, morse_alphabet[i]) == 0) {
@@ -85,6 +72,7 @@ void decode_morse(char *sequence) {
 			return;
 		}
 	}
+
 	//loop to print 0-9
 	for (int i = 0; i < 10; i++) {
 		if (strcmp(sequence, morse_numbers[i]) == 0) {
@@ -93,12 +81,15 @@ void decode_morse(char *sequence) {
 			return;
 		}
 	}
-
-	
 }
 
 ISR(TIMER1_COMPA_vect) {
-	unsigned char current_button_state = (PIND & (1 << BUTTON_PIN)) ? 1 : 0; //check button state
+	static unsigned char last_button_state = 0;
+	static volatile int button_press_time_count = 0;
+	static volatile int button_release_time_count = 0;
+
+	//check button state
+	unsigned char current_button_state = (PIND & (1 << BUTTON_PIN)) ? 1 : 0;
 	
 	if (current_button_state == 1) {
 		button_press_time_count++; //keep counting press state
@@ -106,10 +97,9 @@ ISR(TIMER1_COMPA_vect) {
 		} else {
 		button_release_time_count++; //keep counting release state
 		
-		//determine space
 		if (button_release_time_count == SPACE_MIN) {
-			sequence[idx] = ' ';
-			if (idx < 5) idx++;
+			decode_morse(sequence);
+			memset(sequence, 0, sizeof(sequence));  // Reset sequence after decoding
 		}
 	}
 	
@@ -119,14 +109,12 @@ ISR(TIMER1_COMPA_vect) {
 			PORTB |= (1 << WHITE_LED);
 			_delay_ms(50);
 			PORTB &= ~(1 << WHITE_LED);
-			sequence[idx] = '.';
-			if (idx < 5) idx++;
+			strncat(sequence, ".", sizeof(sequence)-1);
 			} else if (button_press_time_count >= DASH_MIN && button_press_time_count < DASH_MAX) {
 			PORTB |= (1 << RED_LED);
 			_delay_ms(50);
 			PORTB &= ~(1 << RED_LED);
-			sequence[idx] = '-';
-			if (idx < 5) idx++;
+			strncat(sequence, "-", sizeof(sequence)-1);
 		}
 		button_press_time_count = 0;
 	}
